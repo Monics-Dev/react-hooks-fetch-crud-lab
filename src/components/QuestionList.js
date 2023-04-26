d199bd545a7ba4be5a2174520eadee9329311928
Monica <monijerobon@gmail.com>
2023-04-26 15:51:38 +0300

    modified files

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,26 @@
-import React from "react";
+
+
+import QuestionItem from "./QuestionItem"
+import React, { useEffect, useState } from "react";
+
 
 function QuestionList() {
-  return (
+  const [Item, setItems] = useState("AllQuestions")
+  
+  useEffect(() => {
+    fetch("http://localhost:5000/Questions")
+    .then(response => response.json())
+    .then(data => (data))
+    .catch(error => console.error(error));
+  }, []);
+
+    return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{
+      
+
+      /* display QuestionItem components here after fetching */}</ul>
     </section>
   );
 }
